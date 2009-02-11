3747c5b495c1ee35e806a58f1f94fd358a12ab61
Rodrigo Coura Torres <rtorres@rct.local>
2009-02-11 17:04:02 -0200

    Changed test reference for validation.

--- script/matlab/ntrain.m
+++ script/matlab/ntrain.m
@@ -1,11 +1,11 @@
-%function [outNet, epoch, trnError, tstError] = ntrain(net, in_trn, out_trn, in_tst, out_tst, trnESize)
+%function [outNet, epoch, trnError, valError] = ntrain(net, in_trn, out_trn, in_val, out_val, trnESize)
 %Trains a neural network using the network structure created by "newff2".
 %Parameters are:
 %	net             -> The neural network structure created by "newff2".
 %	in_trn          -> The input training data.
 %	out_trn         -> The output training data.
-%	in_tst          -> The input testing data.
-%	out_tst         -> The output testing data.
+%	in_val          -> The input validating data.
+%	out_val         -> The output validating data.
 %	trnESize (opt)  -> The number of training events in an epoch.
 %
 %The data presented to the network must be organized so that each column is an event (either input or output),
@@ -13,16 +13,16 @@
 %training events is not specified, the function will use, in each epoch, all the events in the 
 %training group. In all cases, the events are randomly chosen within the training event set.
 %
-%function [outNet, epoch, trnError, tstError] = ntrain(net, in_trn, [], in_tst, [], trnESize)
+%function [outNet, epoch, trnError, valError] = ntrain(net, in_trn, [], in_val, [], trnESize)
 %In this case, the training process will be optimized for pattern recognition problem.
 %Parameters are:
 %	net             -> The neural network structure created by "newff2".
 %	in_trn          -> A cell array containing the input training data of each pattern.
-%	in_tst          -> A cell array containing the input testing data of each pattern.
+%	in_val          -> A cell array containing the input validating data of each pattern.
 %	trnESize (opt)  -> A vector containing the number of training events in an epoch, for each pattern.
 %
 %The desired outputs (target) are internally generated, so, there is no need to provide the training
-%and testing targets which can save a lot of memory. The input training and testing vectors are cell arrays with the
+%and validating targets which can save a lot of memory. The input training and validating vectors are cell arrays with the
 %same size as the number of patterns to be discriminated. Each cell must contain an array with the input events of an
 %specific pattern (where each event is a column). The target output is organized so that the first output node is
 %active for the first cell in the cell array, the second node is active for the second cell in the cell array, and so
@@ -34,5 +34,5 @@
 %	outNet -> The network structure with the new weight values obtained after training.
 %	epoch    -> The epoch values.
 %	trnError -> The training errors obtained for each epoch.
-%	tstError -> The testing errors obtained for each epoch.
+%	valError -> The validating errors obtained for each epoch.
 %

9697daa8de5718c5f4e0965f3a5dfba82a9cb1a6
Rodrigo Coura Torres <rtorres@rct.local>
2009-02-17 19:43:14 -0300

    Right synchronization placed. Now debuging more core dumps.

--- fastnet/matlab/MTHelper.hxx
+++ fastnet/matlab/MTHelper.hxx
@@ -5,21 +5,21 @@
 
 namespace MT
 {
-  pthread_cond_t trnRequest = PTHREAD_COND_INITIALIZER;
-  pthread_cond_t valRequest = PTHREAD_COND_INITIALIZER;
-  pthread_cond_t doneProc[2] = {PTHREAD_COND_INITIALIZER, PTHREAD_COND_INITIALIZER};
-  pthread_cond_t doneValProc[2] = {PTHREAD_COND_INITIALIZER, PTHREAD_COND_INITIALIZER};
-  pthread_mutex_t trnMutex = PTHREAD_MUTEX_INITIALIZER;
-  pthread_mutex_t valMutex = PTHREAD_MUTEX_INITIALIZER;
+  pthread_cond_t trnProcRequest = PTHREAD_COND_INITIALIZER;
+  pthread_cond_t valProcRequest = PTHREAD_COND_INITIALIZER;
+  pthread_mutex_t trnProcMutex = PTHREAD_MUTEX_INITIALIZER;
+  pthread_mutex_t valProcMutex = PTHREAD_MUTEX_INITIALIZER;
+  pthread_cond_t trnGetResRequest = PTHREAD_COND_INITIALIZER;
+  pthread_cond_t valGetResRequest = PTHREAD_COND_INITIALIZER;
+  pthread_mutex_t trnGetResMutex = PTHREAD_MUTEX_INITIALIZER;
+  pthread_mutex_t valGetResMutex = PTHREAD_MUTEX_INITIALIZER;
   pthread_mutex_t dataMutex = PTHREAD_MUTEX_INITIALIZER;
-  pthread_mutex_t doneMutex[2] = {PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER};
-  pthread_mutex_t doneValMutex[2] = {PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER};
 
 
 #ifdef DEBUG
-//  pthread_mutex_t dbMutex = PTHREAD_MUTEX_INITIALIZER;
-//  #define MTDB(dbmsg){pthread_mutex_lock(&dbMutex); {dbmsg}; pthread_mutex_unlock(&dbMutex);}
-  #define MTDB(dbmsg)
+  pthread_mutex_t dbMutex = PTHREAD_MUTEX_INITIALIZER;
+  #define MTDB(dbmsg){pthread_mutex_lock(&dbMutex); {dbmsg}; pthread_mutex_unlock(&dbMutex);}
+//  #define MTDB(dbmsg)
 #else
   #define MTDB(dbmsg)
 #endif
@@ -34,8 +34,24 @@ namespace MT
     unsigned inputSize;
     unsigned outputSize;
     REAL error;
-    bool stop;
+    bool finishThread;
+    bool threadReady;
+    bool analysisReady;
   };
+
+  inline void waitCond(bool &cond, pthread_mutex_t &mutex)
+  {
+    while (true)
+    {
+      pthread_mutex_lock(&mutex);
+      if (cond)
+      {
+        pthread_mutex_unlock(&mutex);
+        break;
+      }
+      pthread_mutex_unlock(&mutex);
+    }
+  }
 
   void *MTValNetwork(void *param)
   {
@@ -45,10 +61,14 @@ namespace MT
     const unsigned incVal = (par->id + 1) * par->inputSize;
     while (true)
     {
-      //Waiting for the signal to start.
-      pthread_mutex_lock(&valMutex);
-      pthread_cond_wait(&valRequest, &valMutex);
-      if (par->stop)
+      //Waiting for waking up...
+      pthread_mutex_lock(&valProcMutex);
+      par->threadReady = true;
+      pthread_cond_wait(&valProcRequest, &valProcMutex);
+      par->threadReady = false;
+      pthread_mutex_unlock(&valProcMutex);
+   
+      if (par->finishThread)
       {
         MTDB(DEBUG1("Exiting validating thread " << par->id));
         pthread_exit(NULL);
@@ -63,11 +83,11 @@ namespace MT
         const REAL *input = &(par->inData[i]);
         const REAL *target = &(par->outData[i]);
         pthread_mutex_unlock(&dataMutex);
-        par->error += par->net->applySupervisedInput(input, target, output);
+//        par->error += par->net->applySupervisedInput(input, target, output);
       }
       MTDB(DEBUG2("Validation process on thread " << par->id << " finished. Waiting for a new epoch..."));
-      pthread_cond_signal(&doneValProc[par->id]);
-      pthread_mutex_unlock(&valMutex);
+      waitCond(par->analysisReady, valGetResMutex);
+      pthread_cond_signal(&valGetResRequest);
     }
   };
 
@@ -80,11 +100,15 @@ namespace MT
     const REAL *output;
 
     while (true)
-    {      
-      //Waiting for the signal to start.
-      pthread_mutex_lock(&trnMutex);
-      pthread_cond_wait(&trnRequest, &trnMutex);
-      if (par->stop)
+    {
+      //Waiting for waking up...
+      pthread_mutex_lock(&trnProcMutex);
+      par->threadReady = true;
+      pthread_cond_wait(&trnProcRequest, &trnProcMutex);
+      par->threadReady = false;
+      pthread_mutex_unlock(&trnProcMutex);
+   
+      if (par->finishThread)
       {
         MTDB(DEBUG1("Exiting training thread " << par->id));
         pthread_exit(NULL);
@@ -99,14 +123,14 @@ namespace MT
         const REAL *input = &(par->inData[i]);
         const REAL *target = &(par->outData[i]);
         pthread_mutex_unlock(&dataMutex);
-        par->error += par->net->applySupervisedInput(input, target, output);
+//        par->error += par->net->applySupervisedInput(input, target, output);
 
         //Calculating the weight and bias update values
-        par->net->calculateNewWeights(output, target);
+//        par->net->calculateNewWeights(output, target);
       }
       MTDB(DEBUG2("Training process on thread " << par->id << " finished. Waiting for a new epoch..."));
-      pthread_cond_signal(&doneProc[par->id]);
-      pthread_mutex_unlock(&trnMutex);
+      waitCond(par->analysisReady, trnGetResMutex);
+      pthread_cond_signal(&trnGetResRequest);
     }
   }
   
@@ -142,7 +166,7 @@ private:
       thp[i].numEvents = numEvents;
       thp[i].inputSize = inputSize;
       thp[i].outputSize = outputSize;
-      thp[i].stop = false; // flag to stop the thread.
+      thp[i].finishThread = thp[i].threadReady = thp[i].analysisReady = false;
       
       const int rc = pthread_create(&th[i], &threadAttr, funcPtr, static_cast<void*>(&thp[i]));
       if (rc) throw "Impossible to create thread! Aborting...";
@@ -157,20 +181,12 @@ public:
             const unsigned inputSize, const unsigned outputSize, const unsigned numThreads = 2)
   {
     nThreads = numThreads;
-    DEBUG1("Creating MTHelper object for " << nThreads << "threads.");
+    DEBUG1("Creating MTHelper object for " << nThreads << " threads.");
 
     //Setting threads for being joinable.
     pthread_attr_init(&threadAttr);
     pthread_attr_setdetachstate(&threadAttr, PTHREAD_CREATE_JOINABLE);
-/*
-    doneMutex = new pthread_mutex_t [nThreads];
-    doneProc = new pthread_cond_t [nThreads];
-    for (unsigned i=0; i<nThreads; i++)
-    {
-      pthread_mutex_init(&doneMutex[i], NULL);
-      pthread_cond_init(&doneProc[i], NULL);
-    }
-*/
+
     //Generating copies of the neural network to be used.
     netVec = new Backpropagation* [nThreads];
     netVec[0] = net;
@@ -183,52 +199,54 @@ public:
   };
 
   virtual ~MTHelper()
-  {
-    pthread_attr_destroy(&threadAttr);
-    
+  {    
     //Ending the threads.
-    for (unsigned i=0; i<nThreads; i++) trnThPar[i].stop = valThPar[i].stop = true;
-    pthread_cond_broadcast(&trnRequest);
-    pthread_cond_broadcast(&valRequest);
-    for (unsigned i=0; i<nThreads; i++) 
+    for (unsigned i=0; i<nThreads; i++)
     {
       void *ret;
+      trnThPar[i].finishThread = valThPar[i].finishThread = true;
+      waitCond(trnThPar[i].threadReady, trnProcMutex);
+      waitCond(valThPar[i].threadReady, valProcMutex);
+      pthread_cond_broadcast(&trnProcRequest);
+      pthread_cond_broadcast(&valProcRequest);    
       pthread_join(trnThreads[i], &ret);
       pthread_join(valThreads[i], &ret);
-      if (i) delete netVec[i];
-      pthread_mutex_destroy(&doneMutex[i]);
-      pthread_cond_destroy(&doneProc[i]);
-      pthread_mutex_destroy(&doneValMutex[i]);
-      pthread_cond_destroy(&doneValProc[i]);
     }
 
-    pthread_mutex_destroy(&trnMutex);
-    pthread_mutex_destroy(&valMutex);
     pthread_mutex_destroy(&dataMutex);
-    pthread_cond_destroy(&trnRequest);
-    pthread_cond_destroy(&valRequest);
+    pthread_cond_destroy(&trnProcRequest);
+    pthread_cond_destroy(&valProcRequest);
+    pthread_mutex_destroy(&trnProcMutex);
+    pthread_mutex_destroy(&valProcMutex);
+    pthread_cond_destroy(&trnGetResRequest);
+    pthread_cond_destroy(&valGetResRequest);
+    pthread_mutex_destroy(&trnGetResMutex);
+    pthread_mutex_destroy(&valGetResMutex);
+    pthread_attr_destroy(&threadAttr);
 
     delete trnThreads;
     delete valThreads;
     delete trnThPar;
     delete valThPar;
     delete netVec;
-//    delete doneMutex;
-//    delete doneProc;
   };
 
   REAL valNetwork()
   {
     REAL gbError = 0.;
-    pthread_cond_broadcast(&valRequest);
+    for (unsigned i=0; i<nThreads; i++) waitCond(valThPar[i].threadReady, valProcMutex);
+    pthread_cond_broadcast(&valProcRequest);
+
     for (unsigned i=0; i<nThreads; i++)
     {
       void *ret;
       DEBUG2("Waiting for validating thread " << i << " to finish...");
-      pthread_mutex_lock(&doneValMutex[i]);
-      pthread_cond_wait(&doneValProc[i], &doneValMutex[i]);
+      pthread_mutex_lock(&valGetResMutex);
+      valThPar[i].analysisReady = true;
+      pthread_cond_wait(&valGetResRequest, &valGetResMutex);
+      valThPar[i].analysisReady = false;
+      pthread_mutex_unlock(&valGetResMutex);
       gbError += valThPar[i].error;
-      pthread_mutex_unlock(&doneValMutex[i]);
     }
     return (gbError / static_cast<REAL>(valThPar[0].numEvents));
   };
@@ -241,17 +259,21 @@ public:
     
     //First we make all the networks having the same training status.
     for (unsigned i=1; i<nThreads; i++) (*netVec[i]) = (*mainNet);
+
+    for (unsigned i=0; i<nThreads; i++) waitCond(trnThPar[i].threadReady, trnProcMutex);
+    pthread_cond_broadcast(&trnProcRequest);
     
-    pthread_cond_broadcast(&trnRequest);
     for (unsigned i=0; i<nThreads; i++)
     {
       void *ret;
       DEBUG2("Waiting for training thread " << i << " to finish...");
-      pthread_mutex_lock(&doneMutex[i]);
-      pthread_cond_wait(&doneProc[i], &doneMutex[i]);
+      pthread_mutex_lock(&trnGetResMutex);
+      trnThPar[i].analysisReady = true;
+      pthread_cond_wait(&trnGetResRequest, &trnGetResMutex);
+      trnThPar[i].analysisReady = false;
+      pthread_mutex_unlock(&trnGetResMutex);
       gbError += trnThPar[i].error;
       if (i) mainNet->addToGradient(*netVec[i]);
-      pthread_mutex_unlock(&doneMutex[i]);
     }
     return (gbError / static_cast<REAL>(trnThPar[0].numEvents));
   }

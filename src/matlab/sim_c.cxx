63de1500fad207b4f2b14c027cb614a4745b5840
Rodrigo Coura Torres <torres@sion.lps.ufrj.br>
2009-04-20 20:38:09 -0300

    nsim using openmp.

--- src/matlab/sim_c.cxx
+++ src/matlab/sim_c.cxx
@@ -7,11 +7,8 @@
  the input data set through the passed neural  network, returning the outputs obtained.
 */
 
-#include <vector>
-#include <iostream>
-#include <fstream>
-#include <iomanip>
 #include <mex.h>
+#include <omp.h>
 
 #include "fastnet/sys/Reporter.h"
 #include "fastnet/neuralnet/feedforward.h"
@@ -32,46 +29,17 @@ const unsigned IN_DATA_IDX = 1;
 /// Index, in the return vector, of the network structure after training.
 const unsigned NET_OUT_IDX = 0;
 
-const unsigned NUM_THREADS = 4;
-
-struct ThreadParams
-{
-  REAL *inputStartAddr;  // Input starting address to start processing.
-  REAL *outputStartAddr;  // Output starting address to start processing.
-  unsigned numEvents; // Total number of events to process.
-  FeedForward *net; // The neural network to use.
-};
-
-
-void *threadRun(void *params)
-{
-  ThreadParams *par = static_cast<ThreadParams*>(params);
-  FeedForward *net = par->net;
-  const unsigned inputSize = (*net)[0];
-  const unsigned outputSize = (*net)[par->net->getNumLayers()-1];
-  const unsigned numBytes2Copy = outputSize * sizeof(REAL);
-  for (unsigned i=0; i<par->numEvents; i++)
-  {
-    memcpy(par->outputStartAddr, par->net->propagateInput(par->inputStartAddr), numBytes2Copy);
-    par->inputStartAddr += inputSize;
-    par->outputStartAddr += outputSize;
-  } 
-  pthread_exit(NULL);
-}
 
 /// Matlab 's main function.
 void mexFunction(int nargout, mxArray *ret[], int nargin, const mxArray *args[])
 {
   try
-  {  
+  {
     //Verifying if the number of input parameters is ok.
     if (nargin != NUM_ARGS) throw "Incorrect number of arguments! See help for information!";
 
-    //Reading the configuration structure
-    const mxArray *netStr = args[NET_STR_IDX];
-    
     // Creating the neural network to use.
-    FeedForward net(netStr);
+    FeedForward net(args[NET_STR_IDX]);
 
     //Checking if the input and output data sizes match the network's input layer.
     if (mxGetM(args[IN_DATA_IDX]) != net[0])
@@ -81,39 +49,22 @@ void mexFunction(int nargout, mxArray *ret[], int nargin, const mxArray *args[])
     const unsigned numEvents = mxGetN(args[IN_DATA_IDX]);
     const unsigned inputSize = mxGetM(args[IN_DATA_IDX]);
     const unsigned outputSize = net[net.getNumLayers()-1];
+    const unsigned numBytes2Copy = outputSize * sizeof(REAL);
     REAL *inputEvents = static_cast<REAL*>(mxGetData(args[IN_DATA_IDX]));
     mxArray *outData = mxCreateNumericMatrix(outputSize, numEvents, REAL_TYPE, mxREAL);
     REAL *outputEvents = static_cast<REAL*>(mxGetData(outData));
+    
+    int i;
+    int chunk = 1000;
+    #pragma omp parallel shared(inputEvents,outputEvents,chunk) private(i) firstprivate(net)
+    {
+      #pragma omp for schedule(dynamic,chunk) nowait
+      for (i=0; i<numEvents; i++)
+      {
+        memcpy(&outputEvents[i*outputSize], net.propagateInput(&inputEvents[i*inputSize]), numBytes2Copy);
+      }
+    }
 
-    pthread_t threads[NUM_THREADS];
-    pthread_attr_t attr;
-    //Setando as threads p/ serem joinable.
-    pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-    ThreadParams *params = new ThreadParams[NUM_THREADS];
-    const unsigned evPerThread = numEvents / NUM_THREADS;
-    for (unsigned i=0; i<NUM_THREADS; i++)
-    {
-      params[i].net = new FeedForward(net);
-      //The first thread takes the remaining elements, in case of odd division.
-      params[i].numEvents = (i) ? evPerThread : (evPerThread + (numEvents % NUM_THREADS));
-      params[i].inputStartAddr = inputEvents;
-      params[i].outputStartAddr = outputEvents;
-      inputEvents += (params[i].numEvents * inputSize);
-      outputEvents += (params[i].numEvents * outputSize);
-      const int rc = pthread_create(&threads[i], &attr, threadRun, (void *)&params[i]);
-      if (rc) throw "Impossible to create thread! Aborting...";
-    }
-    
-    //Free attribute and wait for the other threads
-    pthread_attr_destroy(&attr);
-    for (unsigned i=0; i<NUM_THREADS; i++)
-    {
-      void *ret;
-      pthread_join(threads[i], &ret);
-      delete params[i].net;
-    }
-    
     ret[NET_OUT_IDX] = outData;
   }
   catch (const char *msg) FATAL(msg);

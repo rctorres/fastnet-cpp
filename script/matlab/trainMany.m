d17ded1274fc62dcf69733242bb9c1683e62ee84
Rodrigo Coura Torres <torres@meyrin..lps.ufrj.br>
2009-04-30 16:02:41 -0300

    Added batchSize to the function.

--- script/matlab/trainMany.m
+++ script/matlab/trainMany.m
@@ -1,5 +1,5 @@
-function [oNet, I] = trainMany(net, inTrn, inVal, inTst, numTrains)
-%function [oNet, I] = trainMany(net, inTrn, inVal, inTst, numTrains)
+function [oNet, I] = trainMany(net, inTrn, inVal, inTst, numTrains, batchSize)
+%function [oNet, I] = trainMany(net, inTrn, inVal, inTst, numTrains, batchSize)
 %Returns the maximum SP value obtained for each training, as well as the
 %trained network for each iteration. The function receives a non trained 
 %(but configured) neural network net, the training 
@@ -8,14 +8,21 @@ function [oNet, I] = trainMany(net, inTrn, inVal, inTst, numTrains)
 %regenerated, to test how a network behaves for numTrains trains. oNEt is a structured
 %vector containning the max sp obtained for the network, the trained network structure,
 %the epochs evolution, and the training and validation errors obtained for each epoch.
-%"I" is the index within the oNet cell vector where the best train was achieved.
+%"I" is the index within the oNet cell vector where the best train was achieved. Finally,
+% batchSize is the number of events to use, for each epoch. If not specified, 10000 events will
+%be used, per epoch.
+%
+
+if nargin == 5,
+  batchSize = 10000;
+end
 
 oNet = cell(1,numTrains);
 spVec = zeros(1, numTrains);
 
 for i=1:numTrains,
   net = scrambleWeights(net);
-  [aux.net, aux.epoch, aux.trnError, aux.valError] = ntrain(net, inTrn, inVal);
+  [aux.net, aux.epoch, aux.trnError, aux.valError] = ntrain(net, inTrn, inVal, batchSize);
   aux.sp = calcSP(diag(genConfMatrix(nsim(aux.net, inTst))));
   spVec(i) = aux.sp;
   oNet{i} = aux;

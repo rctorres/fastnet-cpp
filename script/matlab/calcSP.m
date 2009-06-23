d9a80eba46de66ff4436d4a0c7995a00319b9ef8
Rodrigo Coura Torres <torres@sion.lps.ufrj.br>
2009-06-22 22:05:05 -0300

    Consertado texto do help.

--- script/matlab/calcSP.m
+++ script/matlab/calcSP.m
@@ -1,5 +1,5 @@
 function ret  = calcSP(effic)
-%function ret  = calcSP(effic, dim) - Calculates the normalized [0,1] SP value.
+%function ret  = calcSP(effic) - Calculates the normalized [0,1] SP value.
 %effic is a vector containing the detection efficiency [0,1] of each
 %discriminating pattern. It effic is a Matrix, the SP will be calculated 
 %for each collumn.

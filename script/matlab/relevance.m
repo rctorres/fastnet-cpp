d334d8f0378ecf82f16468eaa8c2cd06b52647b8
Rodrigo Coura Torres <torres@meyrin.lps.ufrj.br>
2009-09-29 21:59:53 -0300

    Forcing threshold to be always zero.

--- script/matlab/relevance.m
+++ script/matlab/relevance.m
@@ -1,7 +1,7 @@
-function r = relevance(net, data)
-%function r = relevance(net, data)
+function r = relevance(net, trn, val)
+%function r = relevance(net, trn, val)
 %Perform relevance analysis. net can be iether a projection vector/matrix,
-%or a neural network structure. data can be a matrix, in which case the
+%or a neural network structure. trn/val can be a matrix, in which case the
 %relevance calculation will be done via MSE, or a cell vector. If a cell
 %vector (where each cell is the data corresponding to one pattern) is
 %provided, then the relevance analysis will be done by SP. The function
@@ -10,19 +10,20 @@ function r = relevance(net, data)
 %WARNING: THIS FUNCTION WORKS FOR 2 CLASSES CASE ONLY
 %
 
-  if iscell(data),
-    r = do_by_sp(net, data);
+  if iscell(trn),
+    mdata = mean(cell2mat(trn), 2);
+    r = do_by_sp(net, val, mdata);
   else
-    r = do_by_mse(net, data);
+    mdata = mean(trn, 2);
+    r = do_by_mse(net, val, mdata);
   end
 
 
-function r = do_by_mse(net, data)
+function r = do_by_mse(net, data, mdata)
   %Does relevance by MSE.
   nDim = size(data,1);
   r = zeros(1,nDim);
   out_ref = get_output(net, data);
-  mdata = mean(data, 2);
   
   for i=1:nDim,
      aux = data;
@@ -32,19 +33,17 @@ function r = do_by_mse(net, data)
   end
 
   
-function r = do_by_sp(net, data)
+function r = do_by_sp(net, data, mdata)
   %Does relevance by SP.
-  nROC = 200;
   nDim = size(data{1},1);
   r = zeros(1,nDim);
-  sp_ref = max(genROC(get_output(net, data{1}), get_output(net, data{2}), nROC));
-  mdata = mean(cell2mat(data), 2);
+  sp_ref = get_sp(get_output(net, data{1}), get_output(net, data{2}))
   
   for i=1:nDim,
      aux = data;
      aux{1}(i,:) = mdata(i);
      aux{2}(i,:) = mdata(i);
-     sp = max(genROC(get_output(net, aux{1}), get_output(net, aux{2}), nROC));
+     sp = get_sp(get_output(net, aux{1}), get_output(net, aux{2}))
      r(i) = sp_ref - sp;
   end
 
@@ -59,3 +58,10 @@ function out = get_output(net, data)
   else
     out = nsim(net, data);
   end
+
+  
+  function sp = get_sp(signal, noise)
+    [det, fa] = getEff(signal, noise, 0);
+    sp = calcSP([det, (1-fa)]);
+    
+    

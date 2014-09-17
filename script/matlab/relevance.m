1e24cdcde2893b5b4ed830f3fd197e54244d77c0
Rodrigo Coura Torres <rtorres@rct.local>
2014-09-17 18:12:14 -0300

    Fixed bug in relevance function by SP. The cutting threshold was not being taken into account.

--- script/matlab/relevance.m
+++ script/matlab/relevance.m
@@ -1,19 +1,23 @@
-function r = relevance(net, trn, val)
-%function r = relevance(net, trn, val)
+function r = relevance(net, trn, val, cut)
+%function r = relevance(net, trn, val, cut)
 %Perform relevance analysis. net can be iether a projection vector/matrix,
 %or a neural network structure. trn/val can be a matrix, in which case the
 %relevance calculation will be done via MSE, or a cell vector. If a cell
 %vector (where each cell is the data corresponding to one pattern) is
-%provided, then the relevance analysis will be done by SP. The function
-%returns the output goal (MSE or SP) deviation for each removed input.
+%provided, then the relevance analysis will be done by SP.
+%cut (optional with default to zero) is the cut threshold between classes.
+%It is used only for relevance by SP.
+%The function returns the output goal (MSE or SP) deviation for each removed input.
 %
 %WARNING: THIS FUNCTION WORKS FOR 2 CLASSES CASE ONLY
 %
 
+  if nargin < 4, cut = 0; end
+
   if iscell(trn),
     disp('Doing relevance analysis by SP.');
     mdata = mean(cell2mat(trn), 2);
-    r = do_by_sp(net, val, mdata);
+    r = do_by_sp(net, val, mdata, cut);
   else
     disp('Doing relevance analysis by MSE.');
     mdata = mean(trn, 2);
@@ -35,17 +39,17 @@ function r = do_by_mse(net, data, mdata)
   end
 
   
-function r = do_by_sp(net, data, mdata)
+function r = do_by_sp(net, data, mdata, cut)
   %Does relevance by SP.
   nDim = size(data{1},1);
   r = zeros(1,nDim);
-  sp_ref = get_sp(get_output(net, data{1}), get_output(net, data{2}));
+  sp_ref = get_sp(get_output(net, data{1}), get_output(net, data{2}), cut);
   
   for i=1:nDim,
      aux = data;
      aux{1}(i,:) = mdata(i);
      aux{2}(i,:) = mdata(i);
-     sp = get_sp(get_output(net, aux{1}), get_output(net, aux{2}));
+     sp = get_sp(get_output(net, aux{1}), get_output(net, aux{2}), cut);
      r(i) = sp_ref - sp;
   end
 
@@ -62,8 +66,8 @@ function out = get_output(net, data)
   end
 
   
-  function sp = get_sp(signal, noise)
-    [det, fa] = getEff(signal, noise, 0);
+  function sp = get_sp(signal, noise, cut)
+    [det, fa] = getEff(signal, noise, cut);
     sp = calcSP([det, (1-fa)]);
     
     
